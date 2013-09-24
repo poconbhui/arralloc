718f8c67912f8455a217f0d55ecec264b65c3ae1
Padraig O Conbhui <poconbhui@gmail.com>
2013-09-24 06:26:48 +0100

    Fixed silly oversight. Can now allocate data types that are not the same number of bytes as an int.

--- arralloc.c
+++ arralloc.c
@@ -3,18 +3,69 @@
 #include <stdio.h>
 
 
+/* If on an I860 align arrays to cache line boundaries */
+#ifdef I860
+#define MIN_ALIGN 32
+#else
+#define MIN_ALIGN 1
+#endif
+
+
 /**
  * get_alloc_size gets the overall size of memory we need to allocate
  * for all the pointer arrays plus the data.
  */
 static size_t get_alloc_size(size_t sizeof_data, int num_dims, int* dims);
 
+
 /**
  * set_pointers wires up our array indices so that arr[i][k]...[z] points
  * to (&arr[0]...[0])[i*dim_k*...*dim_z + ... + z].
  * That is, it does what we expect.
  */
-static void set_pointers(void** arr, int num_dims, int* dims);
+static void set_pointers(
+    size_t sizeof_data, void** arr, int num_dims, int* dims
+);
+
+
+/**
+ * get_align_size finds the size of the word the data should be
+ * aligned to.
+ */
+static size_t get_align_size(size_t sizeof_data);
+
+
+/**
+ * get_aligned_pointer returns a pointer that has been brought
+ * up the the closest word boundary for a word of size align_size.
+ */
+static void* get_aligned_pointer(void* data, size_t sizeof_data);
+
+
+static size_t get_align_size(size_t sizeof_data) {
+   size_t align_size;
+
+   if( sizeof_data > MIN_ALIGN ) {
+   	align_size = sizeof_data;
+   }
+   else {
+   	align_size = MIN_ALIGN;
+   }
+   while( (align_size % sizeof_data) || (align_size % MIN_ALIGN) ) {
+   	align_size++;
+   }
+
+   return align_size;
+}
+
+
+static void* get_aligned_pointer(void* data, size_t sizeof_data) {
+    int align_size = get_align_size(sizeof_data);
+
+    return (void*)(
+        ( ((long)(data) + (align_size) - 1)/(align_size) )*(align_size)
+    );
+}
 
 
 void** arralloc(size_t sizeof_data, int num_dims, ...) {
@@ -49,7 +100,6 @@ void** varralloc(size_t sizeof_data, int num_dims, va_list dims_vl) {
     }
 
 
-
     /* Find the total allocation size */
     alloc_size = get_alloc_size(sizeof_data, num_dims, dims);
 
@@ -58,7 +108,7 @@ void** varralloc(size_t sizeof_data, int num_dims, va_list dims_vl) {
     arr = (void**) malloc(alloc_size);
 
 
-    set_pointers(arr, num_dims, dims);
+    set_pointers(sizeof_data, arr, num_dims, dims);
 
 
     /* Return the array */
@@ -68,12 +118,11 @@ void** varralloc(size_t sizeof_data, int num_dims, va_list dims_vl) {
 
 
 size_t get_alloc_size(size_t sizeof_data, int num_dims, int* dims) {
+    size_t pointer_array_size;
+    size_t data_array_size;
 
     /* Iterator */
     int i;
-
-    size_t pointer_array_size;
-    size_t data_array_size;
 
 
     /**
@@ -114,12 +163,15 @@ size_t get_alloc_size(size_t sizeof_data, int num_dims, int* dims) {
         data_array_size *= dims[i];
     }
 
-    return pointer_array_size*sizeof(void*) + data_array_size*sizeof_data;
+    return
+        pointer_array_size*sizeof(void*)
+        + data_array_size*sizeof_data
+        + get_align_size(sizeof_data);
 }
 
 
 
-void set_pointers(void** arr, int num_dims, int* dims) {
+void set_pointers(size_t sizeof_data, void** arr, int num_dims, int* dims) {
     int offset=1;
     void*** p = (void***) arr;
 
@@ -169,7 +221,7 @@ void set_pointers(void** arr, int num_dims, int* dims) {
     /* Initialize p[0] for zeroth iteration. */
     p[0] = arr;
 
-    for(d=0; d<num_dims-1; ++d) {
+    for(d=0; d<num_dims-2; ++d) {
         offset *= dims[d];
 
         /* Set p to start of next pointer list */
@@ -184,6 +236,18 @@ void set_pointers(void** arr, int num_dims, int* dims) {
             p[i] = &p[0][i*dims[d+1]];
 
         }
+    }
 
+    /* Final iteration must take data size and alignment into account */
+    d = num_dims-2;
+    offset *= dims[d];
+
+    p = (void***) p[0];
+
+    p[0] = (void**) get_aligned_pointer(&p[offset], sizeof_data);
+    
+    for(i=1; i<offset; ++i) {
+        /* Same as before, but moving along in memory in steps of sizeof_data */
+        p[i] = &p[0][i*dims[d+1]*sizeof_data/sizeof(void*)];
     }
 }
--- arralloc.test.c
+++ arralloc.test.c
@@ -7,10 +7,10 @@
 int main(void){
 
     /* our array pointers */
-    int *array_1d;
-    int **array_2d;
-    int ***array_3d;
-    int ****array_4d;
+    long long *array_1d;
+    long long **array_2d;
+    long long ***array_3d;
+    long long ****array_4d;
 
     /* some iterators */
     int i, j, k, l;
@@ -26,7 +26,7 @@ int main(void){
     fprintf(stderr, "Testing 1d version\n");
 
     /* Allocate the array */
-    array_1d = (int*) arralloc(sizeof(int), 1, 500);
+    array_1d = (long long*) arralloc(sizeof(long long), 1, 500);
 
     /* Set data on the array */
     fprintf(stderr, "Testing setting\n");
@@ -41,7 +41,7 @@ int main(void){
             fprintf(
                 stderr,
                 "Incorrect value: array_1d[%d] is %d. Should be %d\n",
-                i, array_1d[i], i
+                i, (int)array_1d[i], i
             );
 
             exit_value = EXIT_FAILURE;
@@ -62,7 +62,7 @@ int main(void){
     fprintf(stderr, "Testing 2d version\n");
 
     /* Allocate the array */
-    array_2d = (int**) arralloc(sizeof(int), 2, 5,100);
+    array_2d = (long long**) arralloc(sizeof(long long), 2, 5,100);
 
     /* Set data on the array */
     fprintf(stderr, "Testing setting\n");
@@ -81,7 +81,7 @@ int main(void){
             fprintf(
                 stderr,
                 "Incorrect value: array_2d[%d][%d] is %d. Should be %d\n",
-                i, j, array_2d[i][j], i*100+j
+                i, j, (int)array_2d[i][j], i*100+j
             );
 
             exit_value = EXIT_FAILURE;
@@ -92,7 +92,7 @@ int main(void){
             fprintf(
                 stderr,
                 "Incorrect value: (&array_2d[0][0])[%d] is %d. Should be %d\n",
-                i*100+j, (&array_2d[0][0])[i*100+j], i*100+j
+                i*100+j, (int)(&array_2d[0][0])[i*100+j], i*100+j
             );
 
             exit_value = EXIT_FAILURE;
@@ -113,7 +113,7 @@ int main(void){
     fprintf(stderr, "Testing 3d version\n");
 
     /* Allocate the array */
-    array_3d = (int***) arralloc(sizeof(int), 3, 5,100,88);
+    array_3d = (long long***) arralloc(sizeof(long long), 3, 5,100,88);
 
     /* Set data on the array */
     fprintf(stderr, "Testing setting\n");
@@ -134,7 +134,7 @@ int main(void){
             fprintf(
                 stderr,
                 "Incorrect value: array_3d[%d][%d][%d] is %d. Should be %d\n",
-                i, j, k, array_3d[i][j][k], i*100*88 +j*88 + k
+                i, j, k, (int)array_3d[i][j][k], i*100*88 +j*88 + k
             );
 
             exit_value = EXIT_FAILURE;
@@ -148,7 +148,7 @@ int main(void){
                 "Should be %d\n",
 
                 i*100*88 +j*88 + k,
-                (&array_3d[0][0][0])[i*100*88 +j*88 + k],
+                (int)(&array_3d[0][0][0])[i*100*88 +j*88 + k],
                 i*100*88 +j*88 + k
             );
 
@@ -170,7 +170,7 @@ int main(void){
     fprintf(stderr, "Testing 4d version\n");
 
     /* Allocate the array */
-    array_4d = (int****) arralloc(sizeof(int), 4, 5,100,88,7);
+    array_4d = (long long****) arralloc(sizeof(long long), 4, 5,100,88,7);
 
     /* Set data on the array */
     fprintf(stderr, "Testing setting\n");
@@ -195,7 +195,7 @@ int main(void){
                 "Incorrect value: array_4d[%d][%d][%d][%d] is %d. "
                 "Should be %d\n",
 
-                i, j, k, l, array_4d[i][j][k][l],
+                i, j, k, l, (int)array_4d[i][j][k][l],
                 i*100*88*7 +j*88*7 + k*7 + l
             );
 
@@ -213,7 +213,7 @@ int main(void){
                 "Should be %d\n",
 
                 i*100*88*7 +j*88*7 + k*7 + l,
-                (&array_4d[0][0][0][0])[i*100*88*7 +j*88*7 + k*7 + l],
+                (int)(&array_4d[0][0][0][0])[i*100*88*7 +j*88*7 + k*7 + l],
                 i*100*88*7 +j*88*7 + k*7 + l
             );
 
